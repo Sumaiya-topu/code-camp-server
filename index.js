ac5a98ee6032d66d6d3040dfea391fd0649d4e22
Sumaiya-topu <topusumaiya@gmail.com>
2022-10-27 20:34:54 +0600

    fnal touch

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express()
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
+//use cors
 app.use(cors());
 const categories = require('./data/categories.json');
 const courses = require('./data/courses.json');
